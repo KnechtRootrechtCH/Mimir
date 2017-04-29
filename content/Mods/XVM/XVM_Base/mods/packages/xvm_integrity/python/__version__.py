ac1ebecd09cab2ddf3320aaf54ca3971feb2dbe8
Thomas Burkhart <thomas.burkhart@garaio.com>
2017-04-29 15:33:41 +0200

    #10: updated xvm modfiles

--- content/Mods/XVM/XVM_Base/mods/packages/xvm_integrity/python/__version__.py
+++
@@ -1,4 +0,0 @@
-# This file was created automatically from build script
-__revision__ = '7261'
-__branch__ = 'default'
-__node__ = 'd38c34167b203c0c73d3105e3ad24cf096773164'
---
+++ content/Mods/XVM/XVM_Base_Configuration/mods/packages/xvm_main/python/__version__.py
@@ -0,0 +1,4 @@
+# This file was created automatically from build script
+__revision__ = '7446'
+__branch__ = 'default'
+__node__ = '0a791356881ae499fdf2ccc40663abe614c27a9e'
