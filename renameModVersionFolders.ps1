8d6814cca8e154bc00e6e8467cec6e83dc7179ac
KnechtRootrechtCH <github@atheos.cc>
2018-01-13 17:07:05 +0100

    #13: mod folders updated to 0.9.21.0.3

--- renameModVersionFolders.ps1
+++ renameModVersionFolders.ps1
@@ -1,3 +1,3 @@
-$oldVersion = "0.9.17.1"
-$newVersion = "0.9.18.0"
+$oldVersion = "0.9.21.0.2"
+$newVersion = "0.9.21.0.3"
 Get-ChildItem -Recurse | Where-Object {  $_.Name -eq $oldVersion -and $_.PSIsContainer} | ForEach-Object { Rename-Item $_.FullName $newVersion }
