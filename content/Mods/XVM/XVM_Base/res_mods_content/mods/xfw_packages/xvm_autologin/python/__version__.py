aa7152a2f58dc77a1a47a741d4fc26831be65d3b
KnechtRootrechtCH <github@atheos.cc>
2018-02-16 21:19:12 +0100

    #15: xvm updated

--- content/Mods/XVM/XVM_Base/res_mods_content/mods/xfw_packages/xvm_autologin/python/__version__.py
+++ content/Mods/XVM/XVM_Base/res_mods_content/mods/xfw_packages/xvm_autologin/python/__version__.py
@@ -1,5 +1,5 @@
 # This file was created automatically from build script
-__xvm_version__ = '7.4.0'
-__revision__ = '8204'
+__xvm_version__ = '7.4.1'
+__revision__ = '8208'
 __branch__ = 'default'
-__node__ = '0e296e39ed923f3ac8eaadac74958f538b9c221d'
+__node__ = 'cf93da1a1103c9cfe43e6a7d9c272bdbb4335a11'
