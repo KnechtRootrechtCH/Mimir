11ef82add3293cda73070f45a9b46ed128a8a70c
Unknown <github@atheos.cc>
2018-06-17 17:16:13 +0200

    #23: xvm updated to nightly build 8443

--- content/Mods/XVM/XVM_Base/res_mods_content/mods/xfw_packages/xvm_battleresults/python/__version__.py
+++ content/Mods/XVM/XVM_Base/res_mods_content/mods/xfw_packages/xvm_battleresults/python/__version__.py
@@ -1,6 +1,6 @@
 # This file was created automatically from build script
 __xvm_version__ = '7.6.4-dev'
 __wot_version__ = '1.0.2.1'
-__revision__ = '8442'
+__revision__ = '8443'
 __branch__ = 'default'
-__node__ = '8bc2322679ab6bfe87ad3f58d63f2e7dd1d773dd'
+__node__ = '482e587822289c4ac7588857755d42f2231bfe29'
