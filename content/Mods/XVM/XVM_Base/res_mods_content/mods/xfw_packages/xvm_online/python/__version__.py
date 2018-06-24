bb4c22a012e578c7d87dadeb96854a58fd49880a
Unknown <github@atheos.cc>
2018-06-24 21:01:48 +0200

    #23: update xvm to 7.6.5

--- content/Mods/XVM/XVM_Base/res_mods_content/mods/xfw_packages/xvm_online/python/__version__.py
+++ content/Mods/XVM/XVM_Base/res_mods_content/mods/xfw_packages/xvm_online/python/__version__.py
@@ -1,6 +1,6 @@
 # This file was created automatically from build script
-__xvm_version__ = '7.6.4-dev'
+__xvm_version__ = '7.6.5'
 __wot_version__ = '1.0.2.1'
-__revision__ = '8443'
+__revision__ = '8480'
 __branch__ = 'default'
-__node__ = '482e587822289c4ac7588857755d42f2231bfe29'
+__node__ = '19c3e17276bd9119a1ba6b27ab97dee9ae580245'
