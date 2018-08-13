aed05ddf1c274a9389b426f704bad5066613998e
KnechtRootrechtCH <github@atheos.cc>
2018-08-13 14:49:38 +0200

    #25: update xvm to 7.6.11

--- content/Mods/XVM/XVM_Base/res_mods_content/mods/xfw_packages/xvm_hangar/python/__version__.py
+++ content/Mods/XVM/XVM_Base/res_mods_content/mods/xfw_packages/xvm_hangar/python/__version__.py
@@ -1,6 +1,6 @@
 # This file was created automatically from build script
-__xvm_version__ = '7.6.7'
-__wot_version__ = '1.0.2.2'
-__revision__ = '8502'
-__branch__ = 'default'
-__node__ = '961c3624fe3c4ffe861a00993f10a89a39f63d80'
+__xvm_version__ = '7.6.11'
+__wot_version__ = '1.0.2.4'
+__revision__ = '8554'
+__branch__ = 'wot-1.0.2'
+__node__ = 'f0b1bfcf57cfb35e2f713f3da8724279b95bb3fe'
