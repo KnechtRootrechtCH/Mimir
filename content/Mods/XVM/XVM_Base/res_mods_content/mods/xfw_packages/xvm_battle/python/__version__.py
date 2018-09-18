2fd2dd344fee34f81cde668a42d2d29cf030c096
KnechtRootrechtCH <github@atheos.cc>
2018-09-18 16:05:26 +0200

    #26: updated xvm to 7.7.1

--- content/Mods/XVM/XVM_Base/res_mods_content/mods/xfw_packages/xvm_battle/python/__version__.py
+++ content/Mods/XVM/XVM_Base/res_mods_content/mods/xfw_packages/xvm_battle/python/__version__.py
@@ -1,6 +1,6 @@
 # This file was created automatically from build script
-__xvm_version__ = '7.7.0'
+__xvm_version__ = '7.7.1'
 __wot_version__ = '1.1.0'
-__revision__ = '8570'
+__revision__ = '8588'
 __branch__ = 'default'
-__node__ = '850b683a4b00438320d144a93670ef0e0647aa70'
+__node__ = 'b491dfb55e1a8fd79fc366cbe9c2751be1c2ed53'
