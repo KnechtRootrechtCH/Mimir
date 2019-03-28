adabfca5bd20ef9f70c701157838e0d17b2756f9
KnechtRootrechtCH <github@atheos.cc>
2019-03-28 20:41:13 +0100

    #36: xvm updated to 7.8.6

--- content/Mods/XVM/XVM_Base/res_mods_content/mods/xfw_packages/xvm_profiler/python/__version__.py
+++ content/Mods/XVM/XVM_Base/res_mods_content/mods/xfw_packages/xvm_profiler/python/__version__.py
@@ -1,6 +1,6 @@
 # This file was created automatically from build script
-__xvm_version__ = '7.8.5'
-__wot_version__ = '1.4.1.0'
-__revision__ = '8952'
+__xvm_version__ = '7.8.6'
+__wot_version__ = '1.4.1.1'
+__revision__ = '8963'
 __branch__ = 'master'
-__node__ = '4ee48039b14684fa2293dbe8fa178b963010cbbc'
+__node__ = 'a9bfaca5995bfbc5000672081d5b55be50d976a4'
