d6cafe7ee61142b47eb4f475aa1efa94cd6e8d18
Thomas Burkhart <thomas.burkhart@garaio.com>
2019-05-10 13:08:24 +0200

    #38: updated version folders

--- renameModVersionFolders.ps1
+++ renameModVersionFolders.ps1
@@ -1,3 +1,3 @@
-$oldVersion = "1.4.1.1"
-$newVersion = "1.4.1.2"
+$oldVersion = "1.4.1.2"
+$newVersion = "1.5.0.0"
 Get-ChildItem -Recurse | Where-Object {  $_.Name -eq $oldVersion -and $_.PSIsContainer} | ForEach-Object { Rename-Item $_.FullName $newVersion }
