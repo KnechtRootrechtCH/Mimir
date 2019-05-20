8dbf733c051639cc8a22058205fe8c8c1d8390cb
Thomas <github@atheos.cc>
2019-05-20 21:09:03 +0200

    #39: update xvm to 7.9.1

--- content/Mods/XVM/XVM_Base/res_mods_content/mods/xfw_packages/xvm_autologin/python/__version__.py
+++ content/Mods/XVM/XVM_Base/res_mods_content/mods/xfw_packages/xvm_autologin/python/__version__.py
@@ -1,6 +1,6 @@
 # This file was created automatically from build script
-__xvm_version__ = '7.9.0'
-__wot_version__ = '1.5.0.0'
-__revision__ = '9038'
+__xvm_version__ = '7.9.1'
+__wot_version__ = '1.5.0.1'
+__revision__ = '9061'
 __branch__ = 'default'
-__node__ = '9769744c495c417360d4a0157f7c4f40a62febef'
+__node__ = '0cd51fadea25ab5ee1768bceca0d2cf859c77d32'
