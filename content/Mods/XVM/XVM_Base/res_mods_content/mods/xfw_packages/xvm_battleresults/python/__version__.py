4bdeb679860cde24e15e37242df13ff9ce8d7ba2
Thomas <github@atheos.cc>
2019-05-20 21:34:43 +0200

    #40: xvm updated to nightly build 9089

--- content/Mods/XVM/XVM_Base/res_mods_content/mods/xfw_packages/xvm_battleresults/python/__version__.py
+++ content/Mods/XVM/XVM_Base/res_mods_content/mods/xfw_packages/xvm_battleresults/python/__version__.py
@@ -1,6 +1,6 @@
 # This file was created automatically from build script
-__xvm_version__ = '7.9.1'
-__wot_version__ = '1.5.0.1'
-__revision__ = '9061'
+__xvm_version__ = '7.9.3-dev'
+__wot_version__ = '1.5.0.3'
+__revision__ = '9089'
 __branch__ = 'default'
-__node__ = '0cd51fadea25ab5ee1768bceca0d2cf859c77d32'
+__node__ = 'f2edd9b4d0c5bb1b7d308d3914043877e1b4d678'
