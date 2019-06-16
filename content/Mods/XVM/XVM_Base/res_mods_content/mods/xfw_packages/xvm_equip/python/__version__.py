731e6cdf68d3c6c1f1629ac5ecdfb3b4fc20d88b
Thomas <github@atheos.cc>
2019-06-16 20:25:37 +0200

    #43: xvm updated to 7.9.5

--- content/Mods/XVM/XVM_Base/res_mods_content/mods/xfw_packages/xvm_equip/python/__version__.py
+++ content/Mods/XVM/XVM_Base/res_mods_content/mods/xfw_packages/xvm_equip/python/__version__.py
@@ -1,6 +1,6 @@
 # This file was created automatically from build script
-__xvm_version__ = '7.9.5-dev'
-__wot_version__ = '1.5.1.0'
-__revision__ = '9169'
+__xvm_version__ = '7.9.5'
+__wot_version__ = '1.5.1.1'
+__revision__ = '9179'
 __branch__ = 'default'
-__node__ = '82a40630a6b5b16aff8c5bf65e059649651fff9a'
+__node__ = '4e10e02bf1fba50d807999bd32f98a4f536bebd3'
