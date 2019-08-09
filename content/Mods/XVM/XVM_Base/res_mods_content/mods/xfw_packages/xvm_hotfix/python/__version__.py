b916baeda0307d3e6d39ee19e944f1151097a484
Thomas <github@atheos.cc>
2019-08-09 19:19:40 +0200

    #46: xvm updated to 8.0.0

--- content/Mods/XVM/XVM_Base/res_mods_content/mods/xfw_packages/xvm_hotfix/python/__version__.py
+++ content/Mods/XVM/XVM_Base/res_mods_content/mods/xfw_packages/xvm_hotfix/python/__version__.py
@@ -1,6 +1,6 @@
 # This file was created automatically from build script
-__xvm_version__ = '8.0.0-dev'
+__xvm_version__ = '8.0.0'
 __wot_version__ = '1.6.0.0'
-__revision__ = '9282'
+__revision__ = '9287'
 __branch__ = 'default'
-__node__ = '0489f49cfedb51893e8b24e9ab6ec7d0e21d11ea'
+__node__ = 'fab74c10ceb15124a5290b67c3b6fd852426160e'
