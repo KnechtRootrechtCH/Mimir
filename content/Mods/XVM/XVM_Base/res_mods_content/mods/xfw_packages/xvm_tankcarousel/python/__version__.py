36efce2b9eb1ad0c4eb88bd22b23be592a781b96
Thomas <github@atheos.cc>
2019-09-13 22:30:01 +0200

    #51: xvm updated to nightly build 9340

--- content/Mods/XVM/XVM_Base/res_mods_content/mods/xfw_packages/xvm_tankcarousel/python/__version__.py
+++ content/Mods/XVM/XVM_Base/res_mods_content/mods/xfw_packages/xvm_tankcarousel/python/__version__.py
@@ -1,6 +1,6 @@
 # This file was created automatically from build script
-__xvm_version__ = '8.0.3'
-__wot_version__ = '1.6.0.4'
-__revision__ = '9317'
-__branch__ = 'default'
-__node__ = 'ad2b3647e0c325fe80d19c4733e6f52e039feca2'
+__xvm_version__ = '8.0.4'
+__wot_version__ = '1.6.0.5'
+__revision__ = '9340'
+__branch__ = 'wot-1.6.0'
+__node__ = '530ac341f98f7de60866cc4e7535a7e6470b5d07'
