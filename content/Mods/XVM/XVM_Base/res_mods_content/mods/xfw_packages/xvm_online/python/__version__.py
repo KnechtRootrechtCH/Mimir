c1f11ee3773569e8a4cab27ffea1f373f7d28585
Thomas <github@atheos.cc>
2019-09-16 19:57:16 +0200

    #52: xvm updated to nightly build 9343

--- content/Mods/XVM/XVM_Base/res_mods_content/mods/xfw_packages/xvm_online/python/__version__.py
+++ content/Mods/XVM/XVM_Base/res_mods_content/mods/xfw_packages/xvm_online/python/__version__.py
@@ -1,6 +1,6 @@
 # This file was created automatically from build script
-__xvm_version__ = '8.0.4'
-__wot_version__ = '1.6.0.5'
-__revision__ = '9340'
+__xvm_version__ = '8.0.5'
+__wot_version__ = '1.6.0.6'
+__revision__ = '9344'
 __branch__ = 'wot-1.6.0'
-__node__ = '530ac341f98f7de60866cc4e7535a7e6470b5d07'
+__node__ = '5e4ed21cd8cde372fd4eac6f0d9ec9b042b9616a'
