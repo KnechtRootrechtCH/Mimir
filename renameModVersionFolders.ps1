59a4fa18162bf6361825262b3162e2258bc4d5ca
Thomas <github@atheos.cc>
2019-12-17 21:33:30 +0100

    #57: folders renamed

--- renameModVersionFolders.ps1
+++ renameModVersionFolders.ps1
@@ -1,3 +1,3 @@
-$oldVersion = "1.6.1.3"
-$newVersion = "1.6.1.4"
+$oldVersion = "1.6.1.4"
+$newVersion = "1.7.0.1"
 Get-ChildItem -Recurse | Where-Object {  $_.Name -eq $oldVersion -and $_.PSIsContainer} | ForEach-Object { Rename-Item $_.FullName $newVersion }
